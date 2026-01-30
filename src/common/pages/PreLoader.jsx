5c53e6beca00b5b06853ccdf88f46904da49f85a
emil <emilelizabath2004@gmail.com>
2026-01-30 12:19:47 +0530

    updated loader

--- src/common/pages/PreLoader.jsx
+++ src/common/pages/PreLoader.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 function PreLoader() {
   return (
-    <>
-      <div className='w-full h-full flex justify-center items-center'>
-            <img src='https://mir-s3-cdn-cf.behance.net/project_modules/hd/4cec2c69321565.5b7d0cbe75933.gif' alt='preloader'/>
-      </div>
-    </>
+    <div className="fixed inset-0 flex items-center justify-center bg-white">
+      <img
+        src="https://mir-s3-cdn-cf.behance.net/project_modules/hd/4cec2c69321565.5b7d0cbe75933.gif"
+        alt="preloader"
+        className="w-24 sm:w-32 md:w-40 lg:w-64"
+      />
+    </div>
   )
 }
 
